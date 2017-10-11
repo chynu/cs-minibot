d48a487ba7bdc0ffa4e557facff3153118689f93
Celine <cc972@cornell.edu>
2017-10-11 17:59:34 -0400

    Modified test vision file.

--- basestation/vision/test_vision.py
+++ basestation/vision/test_vision.py
@@ -3,31 +3,29 @@ import numpy as np
 import time
 
 def read_position(img):
-    #bimg = cv2.GaussianBlur(img, (4, 4), 2, 2)
+    # bimg = cv2.GaussianBlur(img, (4, 4), 2, 2)
     gimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     sta = time.time()
     bimg = cv2.medianBlur(gimg, 5)
-#    b,g,r = cv2.split(bimg)
-    circles = cv2.HoughCircles(bimg, cv2.HOUGH_GRADIENT,1,100,
-                            param1=100,param2=60,minRadius=100,maxRadius=0)
+    # b,g,r = cv2.split(bimg)
+    circles = cv2.HoughCircles(bimg, cv2.HOUGH_GRADIENT, 1, 100,
+                param1 = 100, param2 = 60, minRadius = 100, maxRadius = 0)
     
     if circles is not None:
         circles = np.uint16(np.around(circles))
-        
         for i in circles[0,:]: # x, y, r = i
-            cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
-            cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+            cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
     
     return img
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     # Read codes from a camera
     width = 1280
     height = 720
     fps = 30
     
-    vc = cv2.VideoCapture(1)
+    vc = cv2.VideoCapture(0)
     vc.set(cv2.CAP_PROP_FRAME_WIDTH,  width)
     vc.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
     vc.set(cv2.CAP_PROP_FPS, fps)
